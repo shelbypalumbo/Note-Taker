4a444631b5aa89732219e3d2a36b30e03916abc2
shelby palumbo <shelbypalumbo94@gmail.com>
2020-06-11 22:19:18 -0400

    style changes and adding jquery to technologies used

--- README.md
+++ README.md
@@ -1,32 +1,42 @@
 # Note-Taker
+
 A note taking application that allows a user to write to, save, and delete notes.
-<br>
-https://safe-lake-34410.herokuapp.com/
+
+Deployed Application: https://safe-lake-34410.herokuapp.com/
+
 ## User Story
+
 As a student and full time employee, it is important to take notes through the day, so that I can go back to my ideas, grocery lists, etc. when I have the time.
 
-## Usage 
-On the home page, there is a "Get Started" button that redirects the used to the note taking page. 
+## Usage
+
+On the home page, there is a "Get Started" button that redirects the used to the note taking page.
 ![Note Home](assets/css/images/main.png)
 
 #### Write
+
 From this page, a user can write, save, and delete notes!
 ![write](assets/css/images/saved.png)
 
 #### Save
+
 Jot down items in a note for grocery shoppping after work!
 ![two tasks](assets/css/images/save.png)
 Or take down notes to remind yourself about a phone call you have to make later!
 
 #### Delete
+
 Once you no longer need the note, go ahead an click on the trash can to delete it.
 ![delete](assets/css/images/remove.png)
 
 ## Programming Languages Used
-* HTML
-* CSS
-* Javascript
 
-### Additional Styling Framework 
-* Bootstrap | https://getbootstrap.com/
-* Fontsawesome icons | https://fontawesome.com/icons?d=gallery&m=free
+- HTML
+- CSS
+- Javascript
+- jQuery
+
+### Additional Styling Framework
+
+- Bootstrap | https://getbootstrap.com/
+- Fontsawesome icons | https://fontawesome.com/icons?d=gallery&m=free
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
-var $noteTitle = $(".note-title");
-var $noteText = $(".note-textarea");
-var $saveNoteBtn = $(".save-note");
-var $newNoteBtn = $(".new-note");
-var $noteList = $(".list-container .list-group");
+var noteTitle = $(".note-title");
+var noteText = $(".note-textarea");
+var saveNoteBtn = $(".save-note");
+var newNoteBtn = $(".new-note");
+var noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
 var activeNote = {};
@@ -34,26 +34,26 @@ var deleteNote = function(id) {
 
 // If there is an activeNote, display it, otherwise render empty inputs
 var renderActiveNote = function() {
-  $saveNoteBtn.hide();
+  saveNoteBtn.hide();
 
   if (activeNote.id) {
-    $noteTitle.attr("readonly", true);
-    $noteText.attr("readonly", true);
-    $noteTitle.val(activeNote.title);
-    $noteText.val(activeNote.text);
+    noteTitle.attr("readonly", true);
+    noteText.attr("readonly", true);
+    noteTitle.val(activeNote.title);
+    noteText.val(activeNote.text);
   } else {
-    $noteTitle.attr("readonly", false);
-    $noteText.attr("readonly", false);
-    $noteTitle.val("");
-    $noteText.val("");
+    noteTitle.attr("readonly", false);
+    noteText.attr("readonly", false);
+    noteTitle.val("");
+    noteText.val("");
   }
 };
 
 // Get the note data from the inputs, save it to the db and update the view
 var handleNoteSave = function() {
   var newNote = {
-    title: $noteTitle.val(),
-    text: $noteText.val()
+    title: noteTitle.val(),
+    text: noteText.val()
   };
 
   saveNote(newNote).then(function(data) {
@@ -96,16 +96,16 @@ var handleNewNoteView = function() {
 // If a note's title or text are empty, hide the save button
 // Or else show it
 var handleRenderSaveBtn = function() {
-  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
-    $saveNoteBtn.hide();
+  if (!noteTitle.val().trim() || !noteText.val().trim()) {
+    saveNoteBtn.hide();
   } else {
-    $saveNoteBtn.show();
+    saveNoteBtn.show();
   }
 };
 
 // Render's the list of note titles
 var renderNoteList = function(notes) {
-  $noteList.empty();
+  noteList.empty();
 
   var noteListItems = [];
 
@@ -123,7 +123,7 @@ var renderNoteList = function(notes) {
     noteListItems.push($li);
   }
 
-  $noteList.append(noteListItems);
+  noteList.append(noteListItems);
 };
 
 // Gets notes from the db and renders them to the sidebar
@@ -133,14 +133,12 @@ var getAndRenderNotes = function() {
   });
 };
 
-$saveNoteBtn.on("click", handleNoteSave);
-$noteList.on("click", ".list-group-item", handleNoteView);
-$newNoteBtn.on("click", handleNewNoteView);
-$noteList.on("click", ".delete-note", handleNoteDelete);
-$noteTitle.on("keyup", handleRenderSaveBtn);
-$noteText.on("keyup", handleRenderSaveBtn);
-
+saveNoteBtn.on("click", handleNoteSave);
+noteList.on("click", ".list-group-item", handleNoteView);
+newNoteBtn.on("click", handleNewNoteView);
+noteList.on("click", ".delete-note", handleNoteDelete);
+noteTitle.on("keyup", handleRenderSaveBtn);
+noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
-
